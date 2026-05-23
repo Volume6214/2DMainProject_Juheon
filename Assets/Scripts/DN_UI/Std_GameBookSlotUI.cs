b1b71845f91ef977c7d8f11a87fe0b84b0a59c49
Volume <Volume6214@gmail.com>
2026-05-23 18:25:18 +0900

    [UI] 카테고리버튼 기능 구현

--- Assets/Scripts/DN_UI/Std_GameBookSlotUI.cs
+++ Assets/Scripts/DN_UI/Std_GameBookSlotUI.cs
@@ -12,10 +12,11 @@ public class Std_GameBookSlotUI : MonoBehaviour
     [SerializeField] private GameObject Gobj_Selected; // 활성비활성화 기능으로만 이용할거라서 (용도가 이미지가 아니라서)
     [SerializeField] private DaniTechUIButton Button_SlotClick;
 
-    private event Action<string> _onclickSlot;
+    private event Action<string, EGameBookCategoryType> _onclickSlot;
     // private DNItemData _data; // 데이터를 안 가지고 있는이유 = 다양한 데이터들을 도감에서 보여줄 수 있기 때문에 슬롯이 특정 데이터 타입에 종속적이지 않게 하기 위해서, 슬롯이 자기가 어떤 데이터인지 식별할 수 있는 DataId만 보관하는 형태로 구현한다.
 
     private string _slotDataId; // 슬롯이 자기가 살아있는 동안 어떤 슬롯인지 DataId를 보관하는 변수
+    private EGameBookCategoryType _categoryType;
 
     public string GetSlotDataId()
     {
@@ -31,7 +32,7 @@ public class Std_GameBookSlotUI : MonoBehaviour
     public void OnClick_GameBookSlot()
     {
         // 자식이 눌려진걸 부모에게 알림
-        _onclickSlot?.Invoke(_slotDataId);
+        _onclickSlot?.Invoke(_slotDataId, _categoryType);
     }
 
     private void OnDisable()
@@ -39,26 +40,42 @@ public class Std_GameBookSlotUI : MonoBehaviour
         _onclickSlot = null;
     }
 
-    public void InitSlot(string dataId, Action<string> onClickCallBack /*TableType*/) // TODO: 나중에 카테고리에 따라 다른 데이터를 받아올 수 있도록 파라미터를 추가해서 구별할 필요가 있다.
+    private void SetSlotUI(string dataName, string iconPath)
     {
-        var StdBazaarData = DaniTechGameDataManager.Instance.GetStdBazaarItem(dataId);
-        if (StdBazaarData == null) return;
+        Text_MainName.text = dataName; // 이름 반영
+        if (string.IsNullOrEmpty(iconPath) == false) // 아이콘이 있다면
+        {
+            // 이건 잘 만들어 둔거니까 묻지마 사용...< Image에 아이콘, Spitre리소스 불러와서 표기해줄때
+            DaniTechGameUtil.LoadAndSetSpriteImage(Image_MainIcon, iconPath).Forget();
+        }
+    }
+    public void InitSlot(string dataId, EGameBookCategoryType curCategoryType, Action<string, EGameBookCategoryType> onClickCallBack /*TableType*/) // TODO: 나중에 카테고리에 따라 다른 데이터를 받아올 수 있도록 파라미터를 추가해서 구별할 필요가 있다.
+    {
+        if (curCategoryType == EGameBookCategoryType.StdBazaarItem)
+        {
+            var StdBazaarData = DaniTechGameDataManager.Instance.GetStdBazaarItem(dataId);
+            if (StdBazaarData == null) return;
 
-        Text_MainName.text = StdBazaarData.Name;
+            SetSlotUI(StdBazaarData.Name, StdBazaarData.IconPath);
+        }
+        else if (curCategoryType == EGameBookCategoryType.DNItem)
+        {
+            var DNItemData = DaniTechGameDataManager.Instance.GetDNItemData(dataId);
+            if (DNItemData == null) return;
 
-        string defaultIconPath = "Icon/Default_Icon";
+            SetSlotUI(DNItemData.Name, DNItemData.IconPath);
+        }
+        else if (curCategoryType == EGameBookCategoryType.DNMonster)
+        {
+            var DNMonsterData = DaniTechGameDataManager.Instance.GetDNMonsterData(dataId);
+            if (DNMonsterData == null) return;
 
-        string iconPath = string.IsNullOrEmpty(StdBazaarData.IconPath) ? defaultIconPath : StdBazaarData.IconPath;
-
-        //string iconPath = StdBazaarData.IconPath;
-        //if (string.IsNullOrEmpty(StdBazaarData.IconPath) == true) return; // 혹시 누군가 비울 수 있으니 Null체크
-        
-        // 이미지에 아이콘, 스프라이트 소스 불러와서 표기
-        DaniTechGameUtil.LoadAndSetSpriteImage(Image_MainIcon, iconPath).Forget(); // 아이콘이 비어있지 않다면 아이콘을 불러와서 세팅
+            SetSlotUI(DNMonsterData.Name, DNMonsterData.IconPath);
+        }
 
         // 데이터를 잘 받아 왔으면
         _slotDataId = dataId;
-
+        _categoryType = curCategoryType;
         _onclickSlot += onClickCallBack; // 이벤트 등록 완료
 
     }
--- Assets/Scripts/DN_UI/Std_GameBookUI.cs
+++ Assets/Scripts/DN_UI/Std_GameBookUI.cs
@@ -1,45 +1,58 @@
-﻿using NUnit.Framework.Interfaces;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
 
-
+public enum EGameBookCategoryType
+{
+    None = 0,
+    StdBazaarItem,
+    DNItem,
+    DNMonster
+}
 public class Std_GameBookUI : DaniTechUIBase
 {
-    [SerializeField] private GameObject prefab_Slot; //prefab_ 명시
+    [Header("동적 생성할 프리팹")]
+    [SerializeField] private GameObject prefab_Slot; //게임 오브젝트이지만, 프리팹이라는 단어를 명시
 
     [Header("디테일 정보 영역")] //Inspetor 구분용
     [SerializeField] private Image Image_MainIcon;
     [SerializeField] private Text Text_MainName;
     [SerializeField] private Text Text_Description;
 
+    [Header("슬롯 리스트 영역")]
+    [SerializeField] private Transform Transform_SlotRoot; // 스크롤뷰에 슬롯이 생성될 수 있게 위치를 지정하는 것
+
+    [Header("카테고리영역")]
+    [SerializeField] private DaniTechUIButton Button_StdBazaarItemCategory;
+    [SerializeField] private DaniTechUIButton Button_DNItemCategory;
+    [SerializeField] private DaniTechUIButton Button_DNMonsterCategory;
+
     [SerializeField] private DaniTechUIButton Button_CloseUI;
     //[Header("부가 정보")] area하나로 묶어서 껐다켜기
     //[SerializeField] private GameObject Layout_Area;
-
-    [Header("슬롯 리스트 영역")]
-    [SerializeField] private Transform Transform_SlotRoot; // 스크롤뷰에 슬롯이 생성될 수 있게 위치를 지정하는 것
 
     private Dictionary<string, Std_GameBookSlotUI> _slotList = new Dictionary<string, Std_GameBookSlotUI>();
 
     private void OnEnable()
     {
         // 이 UI가 열릴때, 아이템 도감안에 있어야하는 데이터들을 불러온다.
-        ReadItemListAndCreateSlot();
+        Onclick_StdBazaarItem();
 
         Button_CloseUI.BindOnClickButtonEvent(Onclick_CloseGameBookUI);
-    }
-
-    public void Onclick_CloseGameBookUI()
-    {
-        DaniTechUIManager.Instance.CloseUI(DaniTechUIRootType.MainUI, DaniTechUIType.Std_GameBookUI);
+        Button_StdBazaarItemCategory.BindOnClickButtonEvent(Onclick_StdBazaarItem);
+        Button_DNItemCategory.BindOnClickButtonEvent(Onclick_DNItem);
+        Button_DNMonsterCategory.BindOnClickButtonEvent(Onclick_DNMonster);
     }
 
     private void OnDisable()
     {
-        if(_slotList.Count > 0)
+        DestroyAndClearSlotList();
+    }
+
+    private void DestroyAndClearSlotList()
+    {
+        if (_slotList.Count > 0)
         {
             foreach (var slotKv in _slotList)
             {
@@ -50,11 +63,50 @@ public class Std_GameBookUI : DaniTechUIBase
         }
     }
 
-
-    private void ReadItemListAndCreateSlot()
+    public void Onclick_CloseGameBookUI()
     {
-        Debug.Log("디버그테스트");
+        DaniTechUIManager.Instance.CloseUI(DaniTechUIRootType.MainUI, DaniTechUIType.Std_GameBookUI);
+    }
 
+
+
+    public void Onclick_StdBazaarItem()
+    {
+        SetLayoutGameBookByCategory(EGameBookCategoryType.StdBazaarItem);
+    }
+
+    public void Onclick_DNItem()
+    {
+        SetLayoutGameBookByCategory(EGameBookCategoryType.DNItem);
+    }
+
+    public void Onclick_DNMonster()
+    {
+        SetLayoutGameBookByCategory(EGameBookCategoryType.DNMonster);
+    }
+
+
+    public void SetLayoutGameBookByCategory(EGameBookCategoryType categoryType)
+    {
+        DestroyAndClearSlotList();
+
+        switch (categoryType)
+        {
+            case EGameBookCategoryType.StdBazaarItem:
+                ReadStdBazaarItemListAndCreateSlot();
+                break;
+            case EGameBookCategoryType.DNItem:
+                ReadDNItemListAndCreateSlot();
+                break;
+            case EGameBookCategoryType.DNMonster:
+                ReadDNMonsterListAndCreateSlot();
+                break;
+        }
+            
+    }
+
+    private void ReadStdBazaarItemListAndCreateSlot()
+    {
         // 데이터를 읽어와서 순회(foreach)하면서 아이템들을 도감 리스트에 표기
         var dataList = DaniTechGameDataManager.Instance.StdBazaarItemDataList;
         foreach (var dataKv in dataList)
@@ -62,7 +114,49 @@ public class Std_GameBookUI : DaniTechUIBase
             var data = dataKv.Value;
             if (data == null) continue; //데이터가 Null일수 있으니 체크
 
-            CreateGameBookSlot(data.Id);
+            CreateGameBookSlot(data.Id, EGameBookCategoryType.StdBazaarItem);
+        }
+
+        if (_slotList.Count > 0)
+        {
+            foreach (var slotKv in _slotList)
+            {
+                var slot = slotKv.Value;
+                slot.OnClick_GameBookSlot();
+            }
+        }
+    }
+
+    private void ReadDNItemListAndCreateSlot()
+    {
+        var dataList = DaniTechGameDataManager.Instance.ItemDataList;
+        foreach (var dataKv in dataList)
+        {
+            var data = dataKv.Value;
+            if (data == null) continue; 
+
+            CreateGameBookSlot(data.Id, EGameBookCategoryType.DNItem);
+        }
+
+        if(_slotList.Count > 0)
+        {
+            foreach(var slotKv in _slotList)
+            {
+                var slot = slotKv.Value;
+                slot.OnClick_GameBookSlot();
+            }
+        }
+    }
+
+    private void ReadDNMonsterListAndCreateSlot()
+    {
+        var dataList = DaniTechGameDataManager.Instance.MonsterDataList;
+        foreach (var dataKv in dataList)
+        {
+            var data = dataKv.Value;
+            if (data == null) continue;
+
+            CreateGameBookSlot(data.Id, EGameBookCategoryType.DNMonster);
         }
 
         if(_slotList.Count > 0)
@@ -77,7 +171,7 @@ public class Std_GameBookUI : DaniTechUIBase
 
 
     // 슬롯 1개 제대로 생서해주는 메서드
-    private void CreateGameBookSlot(string dataId)
+    private void CreateGameBookSlot(string dataId, EGameBookCategoryType curCategoryType)
     {
         var gObj = Instantiate(prefab_Slot, Transform_SlotRoot);
         if (gObj == null) return;
@@ -87,20 +181,50 @@ public class Std_GameBookUI : DaniTechUIBase
         if (slotComponent == null) return;
 
         //동적 생서된 자식 슬롯(게임오브젝트)안에 있는 컴포넌트를 가져왔다.
-        slotComponent.InitSlot(dataId, OnClickChildSlotSelected);
+        slotComponent.InitSlot(dataId, curCategoryType ,OnClickChildSlotSelected);
         _slotList.Add(dataId, slotComponent);
 
     }
 
-    private void OnClickChildSlotSelected(string slotDataId)
+    private void SetDetailInfoUI(string dataName, string dataDescription = "", string iconPath = "")
     {
-        var currentSelectedData = DaniTechGameDataManager.Instance.GetStdBazaarItem(slotDataId);
-        if (currentSelectedData == null) return;
-        Text_MainName.text = currentSelectedData.Name;
-        Text_Description.text = currentSelectedData.Description;
-        //Text_SellingPrice.text = currentSelectedData.SellingPrice;
-        //Text_SellingPricce.gameObject.SetActive(currentSelectedData.SellingPrice > 0);
-        DaniTechGameUtil.LoadAndSetSpriteImage(Image_MainIcon, currentSelectedData.IconPath).Forget(); // 아이콘이 비어있지 않다면 아이콘을 불러와서 세팅
+        // Text_SellingPrice.text = currentSelectedData.SellingPrice;
+        // Text_SellingPrice.gameObject.SetActive(currentSelectedData.SellingPrice > 0);
+        Text_MainName.text = dataName;
+        Text_Description.text = dataDescription;
+        if (string.IsNullOrEmpty(iconPath) == false)
+        {
+            DaniTechGameUtil.LoadAndSetSpriteImage(Image_MainIcon, iconPath).Forget();
+        }
+    }
+
+    private void OnClickChildSlotSelected(string slotDataId, EGameBookCategoryType categoryType)
+    {
+        if (categoryType == EGameBookCategoryType.StdBazaarItem)
+        {
+            var currentSelectedData = DaniTechGameDataManager.Instance.GetStdBazaarItem(slotDataId);
+            if (currentSelectedData == null) return;
+            SetDetailInfoUI(currentSelectedData.Name, currentSelectedData.Description, currentSelectedData.IconPath);
+
+            foreach (var slotKv in _slotList)
+            {
+                var slot = slotKv.Value;
+                var dataId = slot.GetSlotDataId();
+                slot.SetSelectedUI(slotDataId == dataId);
+            }
+        }
+        else if (categoryType == EGameBookCategoryType.DNItem)
+        {
+            var currentSelectedData = DaniTechGameDataManager.Instance.GetDNItemData(slotDataId);
+            if (currentSelectedData == null) return;
+            SetDetailInfoUI(currentSelectedData.Name, currentSelectedData.Description, currentSelectedData.IconPath);
+        }
+        else if (categoryType == EGameBookCategoryType.DNMonster)
+        {
+            var currentSelectedData = DaniTechGameDataManager.Instance.GetDNMonsterData(slotDataId);
+            if (currentSelectedData == null) return;
+            SetDetailInfoUI(currentSelectedData.Name, currentSelectedData.Description, currentSelectedData.IconPath);
+        }
 
         foreach (var slotKv in _slotList)
         {
